27181f5a2159b3b7991826008b141778ac923708
Dario La Tegola <dariolategola@gmail.com>
2022-01-19 16:03:32 +0100

    Generatore completato

---
+++ main.js
@@ -0,0 +1,6 @@
+let nome = prompt ("Come ti chiami?");
+let cognome = prompt ("Qual è il tuo cognome?");
+let colore = prompt ("Qual è il tuo colore preferito?");
+const numeroFisso = 21;
+
+document.getElementById ("password").innerHTML = "La tua password è " + nome + cognome + colore + numeroFisso;
